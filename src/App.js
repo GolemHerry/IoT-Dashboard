68206817aeac3a82c00ed6e3230b6e78cc518728
GolemHerry <Forsakenhale@gmail.com>
2019-04-02 17:17:54 +0800

    c

--- src/App.js
+++ src/App.js
@@ -2,13 +2,12 @@ import React from 'react';
 //import Login from "./component/Login";
 import Paperbase from "./component/material-ui/Paperbase";
 //import Tables from "./component/Tables";
-import Steps from "./component/register/Steps";
 class App extends React.Component {
   render() {
     return (
       <div className="App">
         <Paperbase />
-        <Steps />
+ 
       </div>
     );
   }
---
+++ src/component/register/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Reg_err from './Reg_err';
+import Steps from './Steps';
+
+export default class Register extends React.Component {
+    render() {
+        return (
+          <div>
+            <Steps />
+          </div>
+        );
+      }
+}
